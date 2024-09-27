b9e3427d0d0b22433003bf4ca775377f5d026efa
Brayan Perez <brayanperezbuitrago@gmail.com>
2024-09-27 01:33:17 -0500

    Added styles to green thene

--- src/pages/Home/components/MessageForm.vue
+++ src/pages/Home/components/MessageForm.vue
@@ -12,7 +12,7 @@
             telefono</label>
           <input type="text" name="phoneNumber" id="phoneNumber"
             class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@company.com" required="true" />
+            placeholder="300..." required="true" />
         </div>
         <div>
           <label for="dateToSend" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Fecha</label>
@@ -24,10 +24,10 @@
           <label for="message" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mensaje</label>
           <textarea id="message" rows="4"
             class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="Write your thoughts here..."></textarea>
+            placeholder="Recuerdame que mañana tengo cita medica"></textarea>
         </div>
         <button type="submit"
-          class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
+          class="w-full text-white bg-green-500 hover:bg-green-600 focus:ring-4 focus:outline-none focus:ring-green-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">
           Create an account
         </button>
         <p class="text-sm font-light text-gray-500 dark:text-gray-400">
